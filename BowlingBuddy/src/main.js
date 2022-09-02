960cdf3ec5cb89d63a8945e51ab9cd2e50517f72
Steven Eucker <seucker0423@gmail.com>
2022-09-02 11:40:16 -0400

    created electron methods for managing files

--- BowlingBuddy/src/main.js
+++ BowlingBuddy/src/main.js
@@ -1,37 +1,85 @@
-const {app, BrowserWindow} = require('electron')
-    const url = require("url");
-    const path = require("path");
+const {app, BrowserWindow, ipcMain} = require('electron')
+const url = require("url");
+const path = require("path");
+const fs = require("fs");
 
-    let mainWindow
+let mainWindow
 
-    function createWindow () {
-      mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
-      
-      mainWindow.loadURL(
-        path.resolve(
-            path.join(__dirname, `../dist/bowling-buddy/index.html`)
-        )
-      );
-      // Open the DevTools.
-      mainWindow.webContents.openDevTools()
-
-      mainWindow.on('closed', function () {
-        mainWindow = null
-      })
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
     }
+  })
+  
+  mainWindow.loadURL(
+    path.resolve(
+        path.join(__dirname, `../dist/bowling-buddy/index.html`)
+    )
+  );
+  // Open the DevTools.
+  mainWindow.webContents.openDevTools()
 
-    app.on('ready', createWindow)
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+};
 
-    app.on('window-all-closed', function () {
-      if (process.platform !== 'darwin') app.quit()
-    })
+app.on('ready', createWindow);
 
-    app.on('activate', function () {
-      if (mainWindow === null) createWindow()
-    })
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+});
+
+ipcMain.on("GetPath",(event, args) => {
+  event.reply("GetPath-reply", {path: app.getPath(args)});
+});
+
+ipcMain.on("GetAllFiles", (event, args) => {
+  fs.readdir(args, (error, files) => {
+    if(error){
+      event.reply("GetAllFiles-reply", {errorMessage:error, error: true});
+      return;
+    }
+    event.reply("GetAllFiles-reply", {files: files, error:false, errorMessage: null});
+  });
+});
+
+ipcMain.on("ReadFile", (event, args) => {
+  if (!fs.existsSync(args)) {
+    event.reply("ReadFile-reply", {error: true, errorMessage: "File Not Found."});
+    return;
+  }
+
+  var data  = fs.readFileSync(args, 'utf8');
+  event.reply("ReadFile-reply", {fileContent: data, error: false, errorMessage: null});
+});
+
+ipcMain.on("SaveFile", (event, args) => {
+  var folerPath = args[0];
+  var pathToFile = folderPath + "\\" + args[1];
+  var data = args[2];
+
+  try{
+    if (!fs.existsSync(folerPath)) {
+      fs.mkdirSync(folderPath);
+    }
+  
+    if (fs.existsSync(pathToFile)) {
+      fs.appendFileSync(pathToFile, data+"\n");
+      return;
+    } 
+    fs.writeFileSync(pathToFile, data+"\n");
+
+    event.reply("SaveFile-reply", {error: false, errorMessage: null});
+  }
+  catch (error) {
+    event.reply("SaveFile-reply", {error: true, errorMessage: error});
+  }
+});
