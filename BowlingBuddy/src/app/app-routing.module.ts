5a257c37bb7ae03a1c4621a1713df76d6fe1a000
Steven Eucker <seucker0423@gmail.com>
2022-10-15 10:21:55 -0400

    route for league manage page

--- BowlingBuddy/src/app/app-routing.module.ts
+++ BowlingBuddy/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateLeagueComponent } from './pages/create-league/create-league.component';
+import { ManageLeaguesComponent } from './pages/manage-leagues/manage-leagues.component';
 import { ManageTeamsComponent } from './pages/manage-teams/manage-teams.component';
 import { WeeklyTabsComponent } from './pages/weekly-tabs/weekly-tabs.component';
 
 const routes: Routes = [
   { path: 'new-league', component: CreateLeagueComponent },
   { path: 'weekly-tabs', component: WeeklyTabsComponent },
-  { path: 'manage-teams', component: ManageTeamsComponent }
+  { path: 'manage-teams', component: ManageTeamsComponent },
+  { path: 'manage-league', component: ManageLeaguesComponent },
 ];
 
 @NgModule({
